8e219279d380b38c705f3515488cdc90dac8b03e
Mikael Brandt <mikael.brandt90@gmail.com>
2017-04-05 08:17:56 +0200

    Also export the new feature...

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import * as acceptance from './acceptance';
 import * as async from './async';
 import * as fetch from './fetch';
+import * as fetchAsync from './fetch-async';
 import * as xhr from './xhr';
 
 module.exports = {
   ...acceptance,
   ...async,
   ...fetch,
+  ...fetchAsync,
   ...xhr,
 };
