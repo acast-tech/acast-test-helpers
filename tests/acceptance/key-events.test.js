6ff61d7f3d6efa528027a2b1b739bd194995ccfc
Mikael Brandt <mikael.brandt90@gmail.com>
2017-05-11 17:43:42 +0200

    Add basic test for keyEventIn

---
+++ tests/acceptance/key-events.test.js
@@ -0,0 +1,40 @@
+import {
+  click,
+  mouseDown,
+  mouseUp,
+  mouseMove,
+  setupAsync,
+  andThen,
+  keyEventIn,
+  jQuery as $
+} from '../../src';
+
+describe('keyEventIn', () => {
+  setupAsync();
+
+  let element;
+
+  beforeEach(() => {
+    element = document.createElement('input');
+    element.className = 'input-for-key-event-test'
+  });
+
+  afterEach(() => {
+    document.body.removeChild(element);
+    element = null;
+  });
+
+  it('waits for selector to show up and then triggers a key event in it', (done) => {
+    function callback(event) {
+      expect(event.bubbles).to.be.true();
+      expect(event.keyCode).to.equal(39);
+      done();
+    }
+
+    element.addEventListener('keydown', callback);
+
+    keyEventIn('.input-for-key-event-test', 'keydown', 39);
+
+    document.body.appendChild(element);
+  });
+});
